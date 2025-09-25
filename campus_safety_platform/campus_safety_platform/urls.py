7c837fe436ac867d5c8f69acafdb5f2118d641cb
Mahlatsi Mashilo <mahlatsimm1510@gmail.com>
2025-09-25 02:34:44 +0200

    folder restructure

--- campus_safety_platform/campus_safety_platform/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("safety.urls")),  # include your safety app routes
-]
